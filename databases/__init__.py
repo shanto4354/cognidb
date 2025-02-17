7d9bd9c4be3e24281ff28b6abadcd55bee7140f0
shanto4354 <mdshanto2520@gmail.com>
2025-02-17 09:21:42 +0600

    Delete databases directory

--- databases/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_db import BaseDB
-from .postgres_db import PostgresDB
-from .mysql_db import MySQLDB
--- databases/base_db.py
+++
@@ -1,9 +0,0 @@
-class BaseDB:
-    def connect(self):
-        raise NotImplementedError("Connect method not implemented.")
-
-    def fetch_schema(self):
-        raise NotImplementedError("Fetch schema method not implemented.")
-
-    def execute_query(self, query):
-        raise NotImplementedError("Execute query method not implemented.")
